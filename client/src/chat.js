b447c9569bc53ab2256f029a560a66f4a951fabf
Rens Pennings <renspennings@gmail.com>
2021-01-19 15:10:34 +0100

    chat now scrolling by using scrolltop

--- client/src/chat.js
+++ client/src/chat.js
@@ -1,32 +1,43 @@
 import { useSelector } from "react-redux";
 import { socket } from "./socket";
+import { Link } from "react-router-dom";
+import { useEffect, useRef } from "react";
+// import { useRef } from "react";
 
 export default function Chat() {
+    const elemRef = useRef();
     //1. retrieve chat messages from redux and render them
     const chatMessages = useSelector((state) => state && state.messages);
 
+    useEffect(() => {
+        elemRef.current.scrollTop += 200;
+        // console.log("useEffect Runs", elemRef.current.scrollTop);
+    }, [chatMessages]);
+
     const handleKeyDown = (e) => {
         if (e.key === "Enter") {
+            e.preventDefault();
             socket.emit("my new chat message", e.target.value);
+            return (e.target.value = "");
         }
     };
 
     return (
         <div>
             <h1 className="chatH1">Chatroom</h1>
-            <div className="chatContainer">
+            <div ref={elemRef} className="chatContainer">
                 {chatMessages &&
-                    chatMessages.map((message) => (
-                        <div className="chatNode" key={message.id}>
+                    chatMessages.map((message, idx) => (
+                        <div className="chatNode" key={idx}>
                             <img className="chatImg" src={message.url}></img>
                             <p>
-                                {message.first} {message.last} :{" "}
-                                <strong>{message.message}</strong>
+                                <strong>{message.first} </strong>:{" "}
+                                {message.message}
                             </p>
                         </div>
                     ))}
-                <textarea onKeyDown={handleKeyDown} />
             </div>
+            <textarea className="chatBox" onKeyDown={handleKeyDown} />
         </div>
     );
 }
